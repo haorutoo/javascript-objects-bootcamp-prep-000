4e7c30f7cdf9ab41be408cf5028a9617bcf4c4e3
haorutoo <haorutoo@gmail.com>
2017-10-05 13:58:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {artist: "title"}
 
 function updatePlaylist(playlist, artistName, songTitle){
   //object.assign({}, playlist, {artistName:songTitle})
-  playlist = {artistName, 'songTitle'}
+  object.playlist = {artistName, songTitle}
   return playlist
 }
